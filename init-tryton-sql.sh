fd4161d052d9e97016dce7b7cf92d24e744dbe4d
pobsteta <pascal.obstetar@gmail.com>
2018-02-25 18:17:34 +0100

    maj file

--- init-tryton-sql.sh
+++ init-tryton-sql.sh
@@ -3,5 +3,5 @@ set -e
 
 if [ "$REP" ]; then
     cd /data/restore
-    wget -O tryton.sql.gz --no-check-certificate https://139.59.135.212/index.php/s/$REP
+    wget -O tryton.sql.gz --no-check-certificate https://139.59.135.212/index.php/s/$REP/download
 fi
